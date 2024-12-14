37846f6d73bc1323d7fee9800b3ea604710cb03f
Daniel Silverstone <dsilvers@digital-scurf.org>
2024-12-14 22:32:28 +0000

    prompt: Support prompt resetting after 1s
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,19 @@ fn main() {
 __prompter_precmd () {{
     eval "$({exe})"
     print -P -f "\033]0;%s\033\\" "$__prompter_title"
+    TMOUT=$__prompter_tmout
 }}
 
 __prompter_preexec () {{
     local title
     print -v title -P -f "%s" "$__prompter_title"
     print -f "\033]0;%s 🏃‍♂️ %s\033\\" "$title" "$1"
+    unset TMOUT
+}}
+
+TRAPALRM () {{
+    __prompter_precmd
+    zle reset-prompt
 }}
 
 autoload -Uz add-zsh hook
@@ -29,6 +36,7 @@ add-zsh-hook preexec __prompter_preexec
 PS1="{left_prompt}"
 RPS1="{right_prompt}"
 __prompter_title="{title}"
+__prompter_tmout=1
 "#,
             exe = std::env::current_exe().unwrap().display(),
             left_prompt = config.left_prompt(),
